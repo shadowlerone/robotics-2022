771d31638922afdc355df07e2d1cff099e656069
Juolite <adamjjnorris@gmail.com>
2022-01-27 22:15:21 -0500

    test journey text

--- src/Journey.js
+++ src/Journey.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Journey() {
   return (
     <div>
-      <h3>This is the Journey page</h3>
+      <h3>This is the Journey page [This is a test message from Adam. Please ignore.]</h3>
     </div>
   );
 }
