8a9d1b391f2e26e0ad220bbd2a242819bd9cc1f3
iyers16 <shanmukh.iyer@gmail.com>
2022-01-28 19:36:35 -0500

    setup title img WARNING: body is permanent

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,24 @@
 import React from "react";
 import './Landing.css';
 // import Helmet from 'react-helmet';
-import Background from '../content/media/images/BG_0.png';
-
+// import Background from '../content/media/images/BG_0.png';
+// import title from '../content/media/images/Items_5.png';
 
 export default function Landing() {
+    
     return (
-        <div style={{ zIndex: "1", height: "100vh", width: "100vw", backgroundImage: `url(${Background})`}}>
-            {/* <style>{'body{background-size: 150px 150px;background-image: linear-gradient(45deg, rgb(255, 0, 221) 25%, transparent 25%, transparent), linear-gradient(-45deg, rgb(255, 0, 221) 25%, transparent 25%, transparent), linear-gradient(45deg, transparent 75%, rgb(255, 0, 221) 75%), linear-gradient(-45deg, #ff7cb5 75%, rgb(255, 0, 221) 75%)}'}</style> */}
-            <h1>Alice</h1><h1 id="hi">in Vanierland</h1>
-            <h3>Content in landing page</h3>
+        <>
             
-        </div>
+            <img className="title" alt="title here" src={require("../content/media/images/Items_5.png")}/>
+                                   
+        </>
+        
     );
 }
+
+
+// useEffect(() => {
+// 	if(someStateYouExpectToChangeToTriggerTheBackgroundChange === 'what you expect it to be to trigger the bg change') {
+// 		document.body.style.backgroundImage = 'url(path/img.png)' // double check my quotes
+// 	}
+// }, [someStateYouExpectToChangeToTriggerTheBackgroundChange])
