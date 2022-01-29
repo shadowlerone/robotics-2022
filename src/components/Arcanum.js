26d92be8ddacbc78987aa02936d10de86ff9e1c2
iyers16 <shanmukh.iyer@gmail.com>
2022-01-29 15:37:23 -0500

    embedded a <iframe> of the playing field

--- src/components/Arcanum.js
+++ src/components/Arcanum.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Arcanum() {
-  return (
-    <div className="Arcanum">
-      <h1>Arcanum</h1>
-    </div>
-  );
+    return (
+        <div className="Arcanum">
+            <h1>Arcanum</h1>
+            <iframe title="Arcanum Playing Field" src="https://myedu11994.autodesk360.com/shares/public/SH35dfcQT936092f0e430c4781ab51ec101b?mode=embed" width="1024" height="600" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>
+        </div>
+    );
 }
