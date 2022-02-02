80191e79df470c275e24fb45a710eebbf6847c88
iyers16 <shanmukh.iyer@gmail.com>
2022-02-02 12:58:00 -0500

    changed to dark landing

--- src/components/Arcanum.js
+++ src/components/Arcanum.js
@@ -7,7 +7,7 @@ function Arcanum({t}) {
             <h1>{t('Arcanum')}</h1>
             <div className="content">
                 <p>{t('placeholder')}</p>
-                <iframe title="Arcanum Playing Field" src="https://myedu11994.autodesk360.com/shares/public/SH35dfcQT936092f0e430c4781ab51ec101b?mode=embed" width="80%" height="50%" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>
+                <iframe title="Arcanum Playing Field" src="https://myedu11994.autodesk360.com/shares/public/SH35dfcQT936092f0e430c4781ab51ec101b?mode=embed" width="80%" height="70%" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>
             </div>
             
         </div>
