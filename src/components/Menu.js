d69a8fedf0d6bca1ce004a5df2a9bcb1ccbc6b90
iyers16 <shanmukh.iyer@gmail.com>
2022-02-11 20:29:29 -0500

    menu page stup + embed map

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,25 +10,32 @@ function Menu({t}) {
             <div className="navbar">
                 <div className="buttons-container">
                     <Link to="/">
-                        <button>{t('Landing')}</button>
+                    <img className="menuitems" alt="cards here" src={require("../assets/media/images/teapot.png")} width="500px"/>
+                        <button className="label" >{t('Landing')}</button>
                     </Link>
                     <Link to="/journey">
-                        <button>{t('Journey')}</button>
+                        <button className="label" >{t('Journey')}</button>
+                        <img className="menuitems" alt="cards here" src={require("../assets/media/images/pinkcup.png")} width="500px"/>
                     </Link>
                     <Link to="/robot">
-                        <button>{t('Robot')}</button>
+                    <img className="menuitems" alt="cards here" src={require("../assets/media/images/watch.png")} width="500px"/>
+                        <button className="label" >{t('Robot')}</button>
                     </Link>
                     <Link to="/arcanum">
-                        <button>{t('Arcanum')}</button>
+                        <button className="label" >{t('Arcanum')}</button>
+                        <img className="menuitems" alt="cards here" src={require("../assets/media/images/hat.png")} width="500px"/>
                     </Link>
                     <Link to="/characters">
-                        <button>{t('Characters')}</button>
+                        <img className="menuitems" alt="cards here" src={require("../assets/media/images/cards.png")} width="500px"/>
+                        <button className="label" >{t('Characters')}</button>
                     </Link>
                     <Link to="/vaniercollege">
-                        <button>{t('VC')}</button>
+                        <button className="label" >{t('VC')}</button>
+                        <img className="menuitems" alt="cards here" src={require("../assets/media/images/bluecup.png")} width="500px"/>
                     </Link>
                     <Link to="/jackpot">
-                        <button>{t('Jackpot')}</button>
+                    <img className="menuitems" alt="cards here" src={require("../assets/media/images/crown.png")} width="500px"/>
+                        <button className="label" >{t('Jackpot')}</button>
                     </Link>
                 </div>
             </div>
