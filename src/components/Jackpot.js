14f3dcf8cc0367d6dce2cee5a00728a0e6eda31f
iyers16 <shanmukh.iyer@gmail.com>
2022-02-14 13:29:41 -0500

    made vcsa banner

--- src/components/Jackpot.js
+++ src/components/Jackpot.js
@@ -3,13 +3,17 @@ import { withNamespaces } from "react-i18next";
 import './Jackpot.css';
 
 function Jackpot({t}) {
+    
     return (
         <div className="Jackpot">
-            <h1>{t('Jackpot')}</h1>
-            <div className="content">
-                <p>{t('placeholder')}</p>
-            </div>
-            
+            <h6>{t('Our sponsor:')}</h6>
+            <br/>
+            <a className="dotWrapper" rel="noreferrer" target="_blank" href="https://www.vcsavanier.com/">
+                <span class="dot">V</span>
+                <span class="dot">C</span>
+                <span class="dot">S</span>
+                <span class="dot">A</span>
+            </a>
         </div>
     );
 }
