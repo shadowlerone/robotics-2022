09c3b819abbc8e64cddc94bbf7f9b0fc8e1fbeaf
iyers16 <shanmukh.iyer@gmail.com>
2022-03-12 19:08:12 -0500

    landing dark attempt

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,22 +6,26 @@ import { withNamespaces } from "react-i18next";
 function Landing({t}) {
     
     return (
-        <div className="Landing">
-            <Link to="/transition">
-                <span className="en">ENGLISH</span>
-                <img className="cake" alt="Cake here" src={require("../assets/media/images/cookie.png")} />
-                
-            </Link>
+        <>
+            <div className="LandingDark"></div>
+            <div className="Landing">
+                <Link to="/transition">
+                    <span className="en">ENGLISH</span>
+                    <img className="cake" alt="Cake here" src={require("../assets/media/images/cookie.png")} />
+                    
+                </Link>
 
-            <a href="https://www.alice-in-wonderland.net/" target="_blank" rel="noreferrer" >
-                <img className="title" alt="title here" src={require("../assets/media/images/title1.png")}/>
-            </a>
+                <a href="https://www.alice-in-wonderland.net/" target="_blank" rel="noreferrer" >
+                    <img className="title" alt="title here" src={require("../assets/media/images/title1.png")}/>
+                </a>
 
-            <Link to="/transition">
-                <img className="vial" alt="vial here" src={require("../assets/media/images/bottle.png")}/>
-                <span className="fr">FRANÇAIS</span>
-            </Link>
-        </div>
+                <Link to="/transition">
+                    <img className="vial" alt="vial here" src={require("../assets/media/images/bottle.png")}/>
+                    <span className="fr">FRANÇAIS</span>
+                </Link>
+            </div>
+        </>
+        
     );
 }
 export default withNamespaces()(Landing);
