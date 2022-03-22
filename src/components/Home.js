da2ff5807a61be1a29f0076c2eb236200154ada6
shadowlerone <andrae.lewis@gmail.com>
2022-03-22 12:45:49 -0400

    started color changins

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,20 @@ import { Link } from "react-router-dom";
 import './Home.css';
 import { withNamespaces } from "react-i18next";
 
+
+
 function Home({t}) {
-    
+    var backgroundToggle = () => {this.setState({dark: true}); console.log("Toggled!")}
+    var state = {
+        dark: false
+    }
     return (
         <>
-            <img className="BGDark" alt="Dark BG here" src={require("../assets/media/images/landing.png")}/>
-            <img className="BGLight" alt="Light BG here" src={require("../assets/media/images/pinklanding.png")}/>
-            <div className="container" >
-                <div className="row1" >
-                    <Link to="/transition">
+            {/* <img className="BGDark" alt="Dark BG here" src={require("../assets/media/images/landing.png")}/>
+            <img className="BGLight" alt="Light BG here" src={require("../assets/media/images/pinklanding.png")}/> */}
+            <div className={`container${state.dark ? " dark" : ""}`} id="container">
+                <div className="row1" onMouseEnter={backgroundToggle}>
+                    <Link to="/transition" className="language_select">
                         <img className="cake" alt="English" src={require("../assets/media/images/en.png")} />
                         <img className="cake" alt="Cake here" src={require("../assets/media/images/cookie.png")} />
                         
@@ -22,8 +27,8 @@ function Home({t}) {
                         <img className="title" alt="title here" src={require("../assets/media/images/title1.png")}/>
                     </a>
                 </div>
-                <div className="row3" >
-                    <Link to="/transition">
+                <div className="row3" onMouseEnter={backgroundToggle}>
+                    <Link to="/transition" className="language_select">
                         <img className="vial" alt="Vial here" src={require("../assets/media/images/bottle.png")}/>
                         <img className="vial" alt="French" src={require("../assets/media/images/fr.png")} />
                     </Link>
